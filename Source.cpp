daefe4ce9d6bd3dcd5697439c1578dc0081254dc
VladL3301 <115415496+VladL3301@users.noreply.github.com>
2022-10-23 19:27:25 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Rus");
+    float sum = 0, r;
+    for (int i = 1; i <= 10; i++) {
+        cin >> r;
+            sum += 1;
+    }
+    cout << sum << endl;
+    int main() {
+        float SUS = 1, r;
+        for (int i = 1; i <= 10; i++) {
+            cin >> r;
+            SUS *= r
+
+            
+        }
+        cout << SUS << endl;
+}
